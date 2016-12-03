9eabbc864d5533473f356e7e5eb4d1049f772619
Tom Insam <tom@movieos.org>
2016-12-03 14:52:04 -0800

    extract service names properly

--- flametouch/ServiceName.swift
+++ flametouch/ServiceName.swift
@@ -17,13 +17,26 @@ class ServiceName {
         "_airport._tcp.",
         "_sleep-proxy._udp.",
         "_ssh._tcp.",
+        "_ipp._tcp.", // printer
+        "_googlecast._tcp.", // chromecast
     ]
     
     static func nameForServiceGroup(_ group : ServiceGroup) -> String {
         // Look for important names first
         for name in IMPORTANT_NAMES {
             for service in group.services {
-                if service.type == name {
+                if service.type == "_googlecast._tcp." {
+                    let map = mapFromService(service)
+                    if let name = map["fn"] {
+                        return name
+                    } else {
+                        return service.name
+                    }
+                    //return mapFromService(service)["fn"] ?? service.name
+                } else if service.type == "_ipp._tcp." && service.name.contains(" @ ") {
+                    // "printer name @ computer name" for windows printer sharing
+                    return service.name.components(separatedBy: " @ ")[1]
+                } else if service.type == name {
                     return service.name
                 }
             }
@@ -31,5 +44,16 @@ class ServiceName {
         // fallback
         return group.services.first!.name
     }
+
+    static func mapFromService(_ service : NetService) -> [String: String] {
+        var map = [String:String]()
+        if let txtRecord = service.txtRecordData() {
+            for (key, value) in NetService.dictionary(fromTXTRecord: txtRecord) {
+                map[key] = (NSString(data: value, encoding: String.Encoding.utf8.rawValue) as! String)
+            }
+        }
+        return map
+    }
+
     
 }
