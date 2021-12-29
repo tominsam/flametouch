7b46ab1d6b5c7fef3de5a330e6d4f85494d780ed
Tom Insam <tom@movieos.org>
2021-12-28 16:42:01 -0800

    Update screenshots

--- Image Resources/Screenshots/Mac/convert.sh
+++ Image Resources/Screenshots/Mac/convert.sh
@@ -1,1 +1,1 @@
-for file in ~/Desktop/*.png; do echo $file; convert $file -gravity center -crop 2560x1600+0+0 +repage $file; done
+for file in ~/Desktop/*.png; do echo "$file"; convert "$file" -gravity center -crop 2560x1600+0+0 +repage "$file"; done
